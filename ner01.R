187012d2de044fd403e600db37a75f45c8d91968
wouwou4444 <valery.buz@outlook.com>
2018-09-02 13:40:37 +0200

    Add examples and fix packaes names

--- ner01.R
+++ ner01.R
@@ -2,8 +2,23 @@ install.packages("rJava")
 
 library(rJava)
 
-install.packages(c("NLP", "openNLP", "Rweka", "qdap"))
+install.packages(c("NLP", "openNLP", "RWeka", "qdap"))
 
 install.packages("StanfordCoreNLPjars.fr", repos = "http://datacube.wu.ac.at",
                  type = "source")
 
+library(NLP)
+library(openNLP)
+library(RWeka)
+
+### Example 1
+txt <- " IBM is an MNC with headquarters in New York. Oracle is a cloud company in California. James works in IBM. Oracle hired John for cloud expertise. They give 100% to their profession"
+str(txt)
+
+
+txt_str <- as.String(txt)
+str(txt_str)
+
+### Example 2
+bio <- readLines("data/nlp/anb-jarena-lee.txt")
+print(bio)
