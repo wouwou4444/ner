3b1164d816e8f4851666c45272b720aeb75bb610
wouwou4444 <valery.buz@outlook.com>
2018-09-02 11:32:53 +0200

    Add install file for NER in R

---
+++ ner01.R
@@ -0,0 +1,9 @@
+install.packages("rJava")
+
+library(rJava)
+
+install.packages(c("NLP", "openNLP", "Rweka", "qdap"))
+
+install.packages("StanfordCoreNLPjars.fr", repos = "http://datacube.wu.ac.at",
+                 type = "source")
+
